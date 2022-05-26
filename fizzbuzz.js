27c055146547579a4c25b33e23ce3e114d57d973
sisko81 <abdurcampbell@yahoo.com>
2022-05-26 15:41:57 +0900

    update

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,1 +1,11 @@
 //Create a fizzbuzz game
+
+let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz up to: "));
+
+for(let i = 1; i <= answer; i++) {
+
+    if(i % 3 === 0) {
+        console.log('Fizz')
+    }
+    console.log(i)
+}
