5d4885b6bcd87c5f7bdedbcdd28fd573da7efd7e
sisko81 <abdurcampbell@yahoo.com>
2022-05-26 16:09:46 +0900

    fizzbuzz complete

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -4,8 +4,16 @@ let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz
 
 for(let i = 1; i <= answer; i++) {
 
-    if(i % 3 === 0) {
+    if(i % 3 === 0 && i % 5 === 0) {
+        console.log('FizzBuzz')
+    }
+    else if(i % 3 === 0) {
         console.log('Fizz')
     }
-    console.log(i)
+    else if(i % 5 === 0) {
+        console.log('Buzz')
+    }
+    else {
+        console.log(i)
+    }
 }
